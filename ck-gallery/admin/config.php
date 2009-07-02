e0755f7137dd7e260fe2802414121ba6bb5051ea
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2009-07-01 21:49:37 -0700

    Added admin/ directory to git

---
+++ ck-gallery/admin/config.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ *  DO NOT EDIT THIS FLE! This is the default config file for the gallery. 
+ *  This file is used if there is a problem with the generated config file.
+ *  In the event your gallery configuration gets messed up, make a copy of 
+ *  this file and rename it to config.php, then refresh your browser.
+ */
+
+$version      = '2.0.0';
+$galleryTitle = 'CK-Gallery v2.0.0';
+$galleryBlurb = 'A simple, yet powerful, PHP photo gallery';
+$thumbSize    = 100;
+$imgPerPage   = 0;
+$cacheExpire  = 0;
+$galleryDir   = 'ck-gallery/gallery';
+$thumbsDir    = 'ck-gallery/gallery/thumbs';
+$logFile      = 'ck-gallery.log';
+$userName     = 'admin';
+$passSalt     = 'R(oD#4o#QnkGjye%';
+$userPass     = 'd285f86c10a5c4f2191def3d209fff05';
+
+?>
